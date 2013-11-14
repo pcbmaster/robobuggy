885c08ef6ecfb0601b97be13d398a08ce406af4e
pcbmaster <fitnessnonsense@gmail.com>
2013-11-13 19:23:46 -0700

    more progress

--- host/main.cpp
+++ host/main.cpp
@@ -5,25 +5,79 @@
 /////////////////////////////////////////*/
 
 #include <stdlib.h>
-#include "seriallib.h"
+#include "serialib.h"
 #include <iostream>
-#include <conio>
+#include <ncurses.h>
+
+using namespace std;
 
 int main(int argc, char* argv[]){
-  if (argc < 1){
+  if (argc < 2){
     cout << "No serial port defined! Exiting.\n";
     return -1;
   }
   else {
+    initscr();
+    timeout(128);
+    noecho();
     char* port = argv[1];
-    seriallib s;
+    serialib s;
     int ret;
     char buf[128];
     ret = s.Open(port, 9600);
     if(ret != 1){
-      cout << "Failed to open " + port + "!\n";
+      printw("Failed to open port!\n");
+      refresh();
+      endwin();
       return ret;
     }
-    cout << "Port " + port + " opened.\n";
+    printw("Port opened.\n");
+    char in;
+    /*
+    while(in != '!'){
+      s.WriteChar('?');
+      in = s.Read(buf, 1, 150);
+    }
+    */
+    while(in != 'q'){
+      switch(getch()){
+      case 'w': //up arrow key
+	s.WriteChar('f');
+	mvprintw(1,0,"Forward");
+	break;
+      case 's': //down arrow key
+	s.WriteChar('b');
+	mvprintw(1,0,"Backward");
+	break;
+      case 'd': //right arrow key
+	s.WriteChar('r');
+	mvprintw(1,0,"Forward Right");
+	break;
+      case 'a': //left arrow key
+	s.WriteChar('l');
+	mvprintw(1,0,"Forward Left");
+	break;
+      case ' ':
+	s.WriteChar('h');
+	mvprintw(1,0,"Brake");
+	break;
+      case 'q':
+	s.WriteChar('b');
+	s.WriteChar('l');
+	mvprintw(1,0,"Backward Left");
+	break;
+      case 'e':
+	s.WriteChar('b');
+	s.WriteChar('r');
+	mvprintw(1,0,"Backward Right");
+	break;
+      case 'p':
+	in = 'q';
+      default:
+	mvprintw(1,0,"Idle      ");
+      }
+    }
+      endwin();
+      cout << "Exiting...\n";
   }
 }
